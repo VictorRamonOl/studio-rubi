4e0fbfe2002766c8b4184e068893b5b900900c55
VictorRamonOl <v.brito.vr@gmail.com>
2026-04-12 08:12:55 -0400

    feat: adicionar Google Ads G-E07KS38SWX

--- app/layout.tsx
+++ app/layout.tsx
@@ -200,9 +200,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
         />
-        {/* Google Analytics GA4 */}
+        {/* Google Analytics GA4 + Google Ads */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-9MRPRX09Y5" />
-        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-9MRPRX09Y5');` }} />
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-9MRPRX09Y5');gtag('config','G-E07KS38SWX');` }} />
 
         {/* TODO: Meta Pixel — substituir XXXXXXXXXXXXXXXXXX pelo seu Pixel ID */}
         {/* <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','XXXXXXXXXXXXXXXXXX');fbq('track','PageView');` }} /> */}
